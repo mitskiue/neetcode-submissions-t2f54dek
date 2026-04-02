eb20539a1d392178a2dd9d2db197c124acd93f0b
Jayrus James <jvombid@up.edu.ph>
2026-04-02 23:57:32 +0800

    Add: products-of-array-discluding-self - submission-10

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-10.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+
+        vector<int> result(nums.size());
+        vector<int> prefix(nums.size()); 
+        vector<int> suffix(nums.size()); 
+
+        prefix[0] = 1;
+
+        for (int i = 1; i < nums.size(); i++) {
+            prefix[i] = prefix[i-1] * nums[i-1];
+        }
+
+        suffix[nums.size()-1] = 1; 
+
+        for (int j = nums.size()-2; j >= 0; j--) {
+            suffix[j] = suffix[j+1] * nums[j+1];
+        }
+        
+        for (int k = 0; k < nums.size(); k++) {
+            result[k] = prefix[k] * suffix[k];
+        }
+
+        return result;
+
+    }
+};
