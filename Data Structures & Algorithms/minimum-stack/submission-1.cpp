3457c44121b346d5212eaa065594e4ca8786e010
Jayrus James <jvombid@up.edu.ph>
2026-04-03 19:56:23 +0800

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cpp
@@ -0,0 +1,37 @@
+class MinStack {
+private:
+    stack<int> minStack;
+    stack<int> myStack;
+public:
+    MinStack() {
+
+    }
+    
+    void push(int val) {
+        myStack.push(val);
+        
+        if (minStack.empty() || val <= minStack.top()) {
+            minStack.push(val);
+        }
+        else {
+            minStack.push(minStack.top());
+        }
+
+        
+    }
+    
+    void pop() {
+        myStack.pop();
+        minStack.pop();
+        
+    }
+    
+    int top() {
+        return myStack.top();
+        
+    }
+    
+    int getMin() {
+        return minStack.top();
+    }
+};
